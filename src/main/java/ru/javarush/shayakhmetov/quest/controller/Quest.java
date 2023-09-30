947100c5f467308f574e40267440b07603e81865
Danil <darkus747474@gmail.com>
2023-09-30 21:21:43 +0300

    Внесены изменения

--- src/main/java/ru/javarush/shayakhmetov/quest/controller/Quest.java
+++ src/main/java/ru/javarush/shayakhmetov/quest/controller/Quest.java
@@ -57,6 +57,7 @@ public class Quest extends HttpServlet {
                 getServletContext().getRequestDispatcher("/finished.jsp").forward(req, resp);
             }
         } else {
+            count = 0;
             getServletContext().getRequestDispatcher("/fail.jsp").forward(req, resp);
         }
     }
